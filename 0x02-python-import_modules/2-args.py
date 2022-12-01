f333b62e6a804828e0fbb8066df199767d8bd868
Idris Muazu Yakub <imuazyakub@gmail.com>
2022-12-01 22:39:09 +0000

    argument

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,10 +5,10 @@ if __name__ == "__main__":
 
     elenum = len(sys.argv) - 1
     if elenum == 0:
-        print("{} arguments:".format(elenum))
+        print("{:d} arguments.".format(elenum))
     elif elenum == 1:
-        print("{} argument:".format(elenum))
+        print("{:d} argument:".format(elenum))
     else:
-        print("{} arguments:".format(elenum))
+        print("{:d} arguments:".format(elenum))
     for i in range(elenum):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
