de236fb1457633b584384fdce309cba44161a48e
Idris Muazu Yakub <imuazyakub@gmail.com>
2022-12-05 21:44:45 +0000

    10 task

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    for i in range(len(my_list)):
-        if my_list[i] % 2 == 0:
-            multiples.append(True)
+    s_copy = my_list[:]
+    for count, i in enumerate(my_list):
+        if i % 2 == 0:
+            s_copy[count] = True
         else:
-            multiples.append(False)
-
-    return (multiples)
+            s_copy[count] = False
+    return(s_copy)
