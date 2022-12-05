62a2d6d72da7c01f873bf38569888a58d55c667a
Idris Muazu Yakub <imuazyakub@gmail.com>
2022-12-05 18:53:46 +0000

    4 task

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if 0 < idx < len(my_list):
-        my_list.append[idx] = element
+    tmp_list = my_list[:]
+    if 0 <= idx < len(my_list):
+        tmp_list[idx] = element
+        return (tmp_list)
     return (my_list)
