d214fb5769cd2651463439def87a67aecd5fc671
Idris Muazu Yakub <imuazyakub@gmail.com>
2022-12-20 18:18:41 +0000

    Taksa 0

--- 0x06-python-classes/0-square.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-
-class Square:
-    """
-    Empty class; that defines Square
-
-    """
-    pass
--- 0x06-python-classes/README.md
+++ 0x06-python-classes/README.md
@@ -1,1 +1,3 @@
 # 0x06. Python - Classes and Objects
+
+In this project, I generally learn;
