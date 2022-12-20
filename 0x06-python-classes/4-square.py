1ecb612598797075d7b9e6a3412bb5229c2153ae
Idris Muazu Yakub <imuazyakub@gmail.com>
2022-12-20 21:18:30 +0000

    Access and update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+"""Define class Square"""
+
+
+class Square:
+    """
+    initializing the class attributes
+    """
+    def __init__(self, size=0):
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """
+        size attribute getter
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """
+        size attribute setter
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        Return the current square area
+        """
+        return (self.size * self.size)
