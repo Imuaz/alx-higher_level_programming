bfe94f9c890ce2572c0974301a44fd2ac02a1d98
Idris Muazu Yakub <imuazyakub@gmail.com>
2022-12-20 18:47:54 +0000

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Define class Square"""
+
+
+class Square:
+    """
+    initializing the class attributes
+    """
+    def __init__(self, size):
+        self.__size = size
