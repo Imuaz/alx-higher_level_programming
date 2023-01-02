be12a02740fafc97692473a07047d562d455c359
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-01-02 19:23:16 +0000

    Task 7

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -70,11 +70,15 @@ class Rectangle:
     def __str__(self):
         """the string that returns a rectangle with character #
         """
-        rep_str = ""
-        if self.__width != 0 and self.__height != 0:
-            rep_str += "\n".join("#" * self.__width
-                                 for i in range(self.__height))
-        return rep_str
+        if self.__width == 0 or self.__height == 0:
+            return ("")
+
+        rect = []
+        for i in range(self.__height):
+            [rect.append(str(self.print_symbol)) for j in range(self.__width)]
+            if i != self.__height - 1:
+                rect.append("\n")
+        return ("".join(rect))
 
     def __repr__(self):
         """Return the string representation of the Rectangle."""
