d900871a3ae3c6e85888dcff737f005105694f76
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-01-03 21:10:15 +0000

    Advance Tasks

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
+
+"""
+Defines a class 'locked class'.
+"""
+
+
 class LockedClass:
+    """
+   class that prevents the user from dynamically creating new instance attributes, except if the new instance attribute 'first_name'
+    """
+
     __slots__ = "first_name"
