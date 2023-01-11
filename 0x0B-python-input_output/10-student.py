bfbacb6bbb649af44eff49e72a07589f7b6d5ba6
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-01-11 06:12:58 +0000

    Tasks

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,23 +1,34 @@
 #!/usr/bin/python3
 """
-A Module Student that defines a student class
+Module that defines the class Student
 """
 
 
 class Student:
     """
-    A class student defines student
+    A Class to create student instances
     """
     def __init__(self, first_name, last_name, age):
-        """
-        __init__ method
-        """
+        """ Special method to initialize """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        """
-        A method to_json
-        """
-        return self.__dict__
+    def to_json(self, attrs=None):
+        """ Method that returns directory description """
+        obj = self.__dict__.copy()
+        if type(attrs) is list:
+
+            for item in attrs:
+                if type(item) is not str:
+                    return obj
+
+            d_list = {}
+
+            for iatr in range(len(attrs)):
+                for satr in obj:
+                    if attrs[iatr] == satr:
+                        d_list[satr] = obj[satr]
+            return d_list
+
+        return obj
