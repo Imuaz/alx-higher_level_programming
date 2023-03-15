4f132812af5be71b774f89f241c49a4d3b9dbc5d
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-03-15 17:56:47 +0100

    find arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-const myVar = process.argv.lenght;
+const myVar = process.argv.length;
 console.log(myVar === 2 ? 'No argument' : myVar === 3 ? 'Argument found' : 'Arguments found');
