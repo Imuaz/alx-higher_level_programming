57478eec976c76550267fdb1e461acf7a66086f4
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-03-15 23:30:10 +0000

    multilang

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-const process = require('process');
-let arg = process.argv[2];
+//const process = require('process');
+const arg = process.argv[2];
 console.log(typeof arg === 'undefined' ? 'No argument' : `${arg}`);
