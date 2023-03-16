600b1f7dc6e9591bd0f05faaa5712e5b0f06a17d
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-03-16 16:33:20 +0000

    tasks 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,8 +3,9 @@ module.exports = class Square extends require('./5-square.js') {
   charPrint (c) {
     if (c === undefined) {
       this.print();
-    } else {
-			let i = 0;
+    }
+    else {
+      let i = 0;
       while (i < this.height) {
         console.log(c.repeat(this.width));
         i++;
