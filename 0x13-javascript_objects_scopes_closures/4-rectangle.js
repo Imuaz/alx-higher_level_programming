5901b444280a1750a8541c6c2a0f6a938a67b35d
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-03-16 11:38:00 +0000

    paste

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.height = h;
+      this.width = w;
+    }
+  }
+
+  print () {
+    let i = 0;
+    let outputs = '';
+    while (i < this.height) {
+      i++;
+      let j = 0;
+      while (j < this.width) {
+        outputs = outputs + 'X';
+        j++;
+      }
+      console.log(outputs);
+      outputs = '';
+    }
+  }
+};
