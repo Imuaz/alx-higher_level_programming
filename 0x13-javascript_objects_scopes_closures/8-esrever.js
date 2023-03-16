692e2b8b57b064d00c14a2decae07082a0037969
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-03-16 22:34:34 +0000

    tasks

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,13 +1,4 @@
 #!/usr/bin/node
-/*exports.esrever = function (list) {
-  const reverseList = [];
-  let i = list.length -1;
-  while (i >= 0) {
-    reverseList.push(list[i]);
-    i--;
-  }
-  return reverseList;
-};*/
 exports.esrever = function (list) {
   return list.reduceRight(function (array, current) {
     array.push(current);
