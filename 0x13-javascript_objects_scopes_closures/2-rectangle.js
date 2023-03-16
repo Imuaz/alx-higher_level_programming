93c343f5ec0cbe97c3ee6beaeb6353089b57ba36
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-03-16 13:39:34 +0100

    task 2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,2 +1,2 @@
 #!/usr/bin/node
-module.exports = class Rectangle { constructor (w, h) { if (w > 0 && h > 0) { this.height = h; this.width = w; } }};
+module.exports = class Rectangle { constructor (w, h) { if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; } }};
