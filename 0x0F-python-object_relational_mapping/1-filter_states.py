80b43baed315c4cf002ffa845952f2f71855cef3
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-03-20 15:36:10 +0100

    readme and Task1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,7 +7,7 @@ import sys
 """
 cript that select all from database
 """
-if __name__ = "__main__":
+if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
