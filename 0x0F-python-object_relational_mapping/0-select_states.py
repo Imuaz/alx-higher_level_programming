802e6f2bc75e55af92096765b1c379bd9084fdce
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-03-20 14:08:46 +0000

    task0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
-"""
-a script that lists all states from the database hbtn_0e_0_usa
-"""
-import MySQLdb
+# Lists all states from the database hbtn_0e_0_usa.
+# Usage: ./0-select_states.py <mysql username> \
+#                             <mysql password> \
+#                             <database name>
 import sys
+import MySQLdb
 
-"""
-script that prints all from database
-"""
-
-if __name__ = "__main__":
+if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT * FROM `states`")
-    [print(state) for state in cur.fetchall()]]
+    c = db.cursor()
+    c.execute("SELECT * FROM `states`")
+    [print(state) for state in c.fetchall()]
