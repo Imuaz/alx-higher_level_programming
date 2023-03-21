3b960a534fd71a6cf29722bc661c3fdce6b9dbfc
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-03-21 08:59:26 +0000

    tasks

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
+"""
+This script takes in an argument and
+displays all values in the states
+where `name` matches the argument
+from the database `hbtn_0e_0_usa`.
+This time the script is safe from
+MySQL injections!
+"""
 
 import MySQLdb
 from sys import argv
 
-"""
-script that lists all states from the database
-"""
 if __name__ == '__main__':
     """
     Access to the database and get the states
