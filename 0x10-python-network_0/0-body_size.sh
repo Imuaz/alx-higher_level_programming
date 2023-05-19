33be161f25f805944ac97dd46534df8096563564
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-05-19 19:59:55 +0000

    readme review and correction

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
 
-# Check if the URL argument is provided
-if [ -z "$1" ]; then
-  echo "Please provide a URL as an argument."
-  exit 1
-fi
-
-# Send a request to the URL and store the response headers
-headers=$(curl -sI "$1")
-
-# Extract the "Content-Length" header value using awk
-size=$(echo "$headers" | awk '/Content-Length/ {print $2}')
-
-# Display the size of the response body
-echo "Size of the response body: $size bytes"
+curl -sI "$1" | awk '/Content-Length/ {print $2}'
 
