63be955b0a2942ab15b47c1a7b432b4d084341ec
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-05-19 22:22:39 +0000

    readme review and correction

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that takes in a URL, sends a request and displays the size
-curl -s -o /dev/null -w "%{http_code}\n" "$1" | grep -q 200 && curl -s "$1"
+curl -sI "$1" | awk '/Content-Length/ {print $2}'
