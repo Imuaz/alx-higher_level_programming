5bd651a8887a589b3d43e87a672c7a543aad30c0
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-05-19 23:29:49 +0000

    readme review and correction

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
-""" Test function find_peak """
-find_peak = __import__('6-peak').find_peak
+"""Module to find the peak in a list of integers"""
 
-print(find_peak([1, 2, 4, 6, 3]))
-print(find_peak([4, 2, 1, 2, 3, 1]))
-print(find_peak([2, 2, 2]))
-print(find_peak([]))
-print(find_peak([-2, -4, 2, 1]))
-print(find_peak([4, 2, 1, 2, 3, 1]))
+
+def find_peak(nums):
+    """find_peak function definition"""
+    size = len(nums)
+    if (size == 0):
+        return
+    mid_index = size // 2
+    if (mid_index == size - 1 or nums[mid_index] >= nums[mid_index + 1]) \
+            and (nums[mid_index] >= nums[mid_index - 1] or mid_index == 0):
+        return nums[mid_index]
+    if (mid_index != size - 1) and nums[mid_index + 1] > nums[mid_index]:
+        return find_peak(nums[mid_index+1:])
+    else:
+        return find_peak(nums[:mid_index])
