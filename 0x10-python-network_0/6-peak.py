f90d60b6b1b0c5c0641a641c16226d411e5b1458
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-05-19 23:54:05 +0000

    readme review and correction

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
+"""Defines a peak-finding algorithm."""
+
 
 def find_peak(list_of_integers):
-    """
-    Find a peak element in a list of unsorted integers.
-
-    Args:
-        list_of_integers (list): List of unsorted integers.
-
-    Returns:
-        int: Peak element in the list, or None if the list is empty.
-
-    """
-    if not list_of_integers:
+    """Return a peak in a list of unsorted integers."""
+    if list_of_integers == []:
         return None
 
     size = len(list_of_integers)
@@ -20,9 +13,8 @@ def find_peak(list_of_integers):
     elif size == 2:
         return max(list_of_integers)
 
-    mid = size // 2
+    mid = int(size / 2)
     peak = list_of_integers[mid]
-
     if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
         return peak
     elif peak < list_of_integers[mid - 1]:
