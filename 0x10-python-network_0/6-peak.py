47ac70d8a98d470866963ca9cfe0db19ac5f1e4c
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-05-19 23:41:31 +0000

    readme review and correction

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,17 +1,28 @@
-#!/usr/bin/python3
-"""Module to find the peak in a list of integers"""
+def find_peak(integer_list):
+    """
+    Find the peak element in a list of unsorted integers.
 
+    Args:
+        integer_list (List[int]): List of unsorted integers.
 
-def find_peak(nums):
-    """find_peak function definition"""
-    size = len(nums)
-    if (size == 0):
-        return
+    Returns:
+        int: Peak element in the list.
+
+    """
+    size = len(integer_list)
+
+    if size == 0:
+        return None
+
     mid_index = size // 2
-    if (mid_index == size - 1 or nums[mid_index] >= nums[mid_index + 1]) \
-            and (nums[mid_index] >= nums[mid_index - 1] or mid_index == 0):
-        return nums[mid_index]
-    if (mid_index != size - 1) and nums[mid_index + 1] > nums[mid_index]:
-        return find_peak(nums[mid_index+1:])
+
+    if (mid_index == size - 1 or integer_list[mid_index] >=
+            integer_list[mid_index + 1]) and \
+            (integer_list[mid_index] >= integer_list[mid_index - 1] or
+             mid_index == 0):
+        return integer_list[mid_index]
+    elif mid_index != size - 1 and integer_list[mid_index + 1] >
+    integer_list[mid_index]:
+        return find_peak(integer_list[mid_index + 1:])
     else:
-        return find_peak(nums[:mid_index])
+        return find_peak(integer_list[:mid_index])
