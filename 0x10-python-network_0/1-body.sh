34aa45c7d810d04029d4e586e31545fddb136ebd
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-05-19 22:32:47 +0000

    readme review and correction

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Get the response body for a given URL for 200 status code responses.
-curl -sL "$1" | awk -v code=200 '/HTTP\/1.1 "code" / {flag=1; next} flag && NF {print}'
+# Script that takes in a URL, sends a request and displays the size
+curl -sI "$1" | awk '/Content-Length/ {print $2}'
