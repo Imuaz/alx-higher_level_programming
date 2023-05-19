9a4518bc0347dda29d92b6f2349658377a142812
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-05-19 23:28:49 +0000

    readme review and correction

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,32 +1,10 @@
 #!/usr/bin/python3
+""" Test function find_peak """
+find_peak = __import__('6-peak').find_peak
 
-def find_peak(list_of_integers):
-    """
-    Find a peak in a list of unsorted integers.
-
-    Args:
-        list_of_integers (List[int]): The list of unsorted integers.
-
-    Returns:
-        int: A peak value from the list, or None if the list is empty.
-    """
-    if not list_of_integers:
-        return None
-
-    size = len(list_of_integers)
-    if size == 1:
-        return list_of_integers[0]
-    elif size == 2:
-        return max(list_of_integers)
-
-    mid = size // 2
-    peak = list_of_integers[mid]
-    left_neighbor = list_of_integers[mid - 1]
-    right_neighbor = list_of_integers[mid + 1]
-
-    if peak > left_neighbor and peak > right_neighbor:
-        return peak
-    elif peak < left_neighbor:
-        return find_peak(list_of_integers[:mid])
-    else:
-        return find_peak(list_of_integers[mid + 1:])
+print(find_peak([1, 2, 4, 6, 3]))
+print(find_peak([4, 2, 1, 2, 3, 1]))
+print(find_peak([2, 2, 2]))
+print(find_peak([]))
+print(find_peak([-2, -4, 2, 1]))
+print(find_peak([4, 2, 1, 2, 3, 1]))
