964e75fc58a68faa502cd078e1ff202fd0238b24
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-05-20 15:27:42 +0000

    readme review and correction

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,16 +7,15 @@ import sys
 import requests
 from requests.auth import HTTPBasicAuth
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     username = sys.argv[1]
     password = sys.argv[2]
 
-    url = f"https://api.github.com/users/{username}"
+    url = f'https://api.github.com/users/{username}'
     response = requests.get(url, auth=HTTPBasicAuth(username, password))
 
     if response.status_code == 200:
-        user_data = response.json()
-        user_id = user_data.get("id")
-        print(f"User ID: {user_id}")
+        data = response.json()
+        print(data.get('id'))
     else:
-        print("Error:", response.text)
+        print(f"Error: {response.status_code}")
