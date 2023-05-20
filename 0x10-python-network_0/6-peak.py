e0f614980a624ff46163dda6106f5b685de20fc8
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-05-20 06:49:32 +0000

    readme review and correction

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,24 +4,25 @@ Module to find the peak in a list of integers
 """
 
 
-def find_peak(Numbers):
+def find_peak(list_of_integers):
     """
     Return a peak in a list of unsorted integers.
     """
-    if Numbers == []:
+    if list_of_integers == []:
         return None
 
-    numSize = len(Numbers)
+    numSize = len(list_of_integers)
     if numSize == 1:
-        return Numbers[0]
+        return list_of_integers[0]
     elif numSize == 2:
-        return max(Numbers)
+        return max(list_of_integers)
 
     midIdx = int(numSize / 2)
-    peak = Numbers[midIdx]
-    if peak > Numbers[midIdx - 1] and peak > Numbers[midIdx + 1]:
+    peak = list_of_integers[midIdx]
+    if peak > list_of_integers[midIdx - 1] and peak >
+    list_of_integers[midIdx + 1]:
         return peak
-    elif peak < Numbers[midIdx - 1]:
-        return find_peak(Numbers[:midIdx])
+    elif peak < list_of_integers[midIdx - 1]:
+        return find_peak(list_of_integers[:midIdx])
     else:
-        return find_peak(Numbers[midIdx + 1:])
+        return find_peak(list_of_integers[midIdx + 1:])
