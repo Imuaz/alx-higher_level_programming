6a595f69f31d42f4774decee3bdf92b8e927447e
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-05-20 12:24:59 +0000

    readme review and correction

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,13 +7,9 @@ import urllib.parse
 import urllib.request
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    email = sys.argv[2]
-
-    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
-    req = urllib.request.Request(url, data=data, method='POST')
-
-    with urllib.request.urlopen(req) as response:
-        body = response.read().decode('utf-8')
-    print("Response body:")
-   print(body)
+    values = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('utf-8')
+    req = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(req) as res:
+        print(res.read().decode('utf-8'))
