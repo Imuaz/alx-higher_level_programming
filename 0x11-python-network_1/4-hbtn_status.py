91366d10b77af199633096f180e0d38813147818
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-05-20 12:51:25 +0000

    readme review and correction

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -12,7 +12,9 @@ if __name__ == "__main__":
 
     data = urllib.parse.urlencode({'email': email}).encode('utf-8')
     req = urllib.request.Request(url, data=data, method='POST')
+
     with urllib.request.urlopen(req) as response:
         body = response.read().decode('utf-8')
+
     print("Response body:")
     print(body)
