0a93a0488fa40976b7f78fe35078ae843595e67b
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-05-20 12:55:34 +0000

    readme review and correction

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,19 +2,12 @@
 """
 Fetches https://intranet.hbtn.io/status.
 """
-import sys
-import urllib.request
+import requests
 
 
-if __name__ == "__main__":
-    url = sys.argv[1]
-    email = sys.argv[2]
-
-    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
-    req = urllib.request.Request(url, data=data, method='POST')
-
-    with urllib.request.urlopen(req) as response:
-        body = response.read().decode('utf-8')
-
-    print("Response body:")
-    print(body)
+if __name__ == '__main__':
+    url = "https://alx-intranet.hbtn.io/status"
+    response = requests.get(url)
+    print("Body response:")
+    print(f"\t- type: {type(response.text)}")
+    print(f"\t- content: {response.text}")
