9e2d58f108284d316b3775f981efb5150f6db9f8
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-05-20 06:25:39 +0000

    readme review and correction

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,17 +7,15 @@ def find_peak(list_of_integers):
     if list_of_integers == []:
         return None
 
-    size = len(list_of_integers)
-    if size == 1:
-        return list_of_integers[0]
-    elif size == 2:
-        return max(list_of_integers)
+    list_of_integers = Numbers
+    size = len(Numbers)
+    return (Numbers[0] if size == 1 else max(Numbers) if size == 2)
 
-    mid = int(size / 2)
-    peak = list_of_integers[mid]
-    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+    midIndx = size // 2
+    peak = Numbers[midIdx]
+    if peak > Numbers[midIdx - 1] and peak > Numbers[midIdx + 1]:
         return peak
-    elif peak < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
+    elif peak < Numbers[midIdx - 1]:
+        return find_peak(Numbers[:midIdx])
     else:
-        return find_peak(list_of_integers[mid + 1:])
+        return find_peak(Numbers[midIdx + 1:])
