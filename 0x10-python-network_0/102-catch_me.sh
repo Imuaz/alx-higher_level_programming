28002e44f6162318518c673c1288c65e5587fee4
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-05-20 07:30:12 +0000

    readme review and correction

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Makes a request to 0.0.0.0:5000/catch_me that gets the message "You got me!".
-curl -s -X POST 0.0.0.0:5000/catch_me --data "name[]=You&name[]=got&name[]=me!"
+curl -s -X PUT -d "You got me!" 0.0.0.0:5000/catch_me
